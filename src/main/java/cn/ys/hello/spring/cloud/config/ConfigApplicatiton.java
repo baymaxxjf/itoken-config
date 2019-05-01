4bbad2844987099b04ce5b1a606ed0e6522e8a7a
baymaxxjf <1340324168@qq.com>
2019-05-01 22:41:49 +0800

    第一提交

---
+++ src/main/java/cn/ys/hello/spring/cloud/config/ConfigApplicatiton.java
@@ -0,0 +1,15 @@
+package cn.ys.hello.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigApplicatiton {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplicatiton.class,args);
+    }
+}
